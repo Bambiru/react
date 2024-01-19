6858f6fc94403583b69b8108fd6234c5b16f72e0
bambiru <dj70083798@gmail.com>
2024-01-19 15:07:08 +0900

    [원론] JSX 공백 문제 - HTML 공백 설정과 비교 - 명시적인 JSX의 공백 설정 방법

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,6 @@
 import React from "https://esm.sh/react";
 import { createRoot } from "https://esm.sh/react-dom";
 
-// HTML vs. JSX ( HTML 아님, 문법은 XML과 유사 )
-
-// 자바스크립트는 예약어를 변수 또는 함수 이름으로 사용할 수 없다 ❌
-// const while = () => { };
-// 객체에서는 예약어를 쓸 수는 있지만, 안쓰는 게 좋다.
-/* const o = {
-  if: "`만약에~`",
-  while: "`~하는 동안`",
-};
-console.log(o["if"], o["while"]); */
-
-// JSX => JSX 변환
-// JSX는 JS로 변환되므로 JSX(결국은 JavaScript)에서는 `예약어`를 사용할 수 없습니다.
-// class, for, if, while, switch, break, ...
-// HTML 속성이 때때로 JavaScript 예약어와 겹치기 때문에 이것이 문제가 됩니다.
-// <div class></div>
-// <label for></label>
-
 const data = {
   greetingMessage: ["hello!", "react."],
   message:
@@ -31,17 +13,11 @@ const createApp = (data) => {
       <h1>
         {data.greetingMessage[0].toUpperCase()}
         <br />
+        {/* 해킹이 아니라, 명시적으로 JSX 구문에서 공백을 설정하는 방법 */}{" "}
+        {/* <br /> */}
         {data.greetingMessage[1].toUpperCase()}
       </h1>
       <p>{data.message}</p>
-
-      <form>
-        {/* JSX:for=>htmlFor, class=>className */}
-        <label htmlFor="searchKeyword" className="sr-only">
-          검색
-        </label>
-        <input id="searchKeyword" type="search" placeholder="검색" />
-      </form>
     </div>
   );
 };
@@ -49,12 +25,4 @@ const createApp = (data) => {
 const rootElement = document.getElementById("root");
 const reactDomRoot = createRoot(rootElement);
 
-reactDomRoot.render(
-  createApp(data, {
-    label: "important",
-    isDisabled: false,
-    min: 0,
-    step: 1,
-    max: 20,
-  })
-);
+reactDomRoot.render(createApp(data));
