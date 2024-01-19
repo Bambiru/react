18d62b888f2a3d23167b19b099475685b29d8cb6
bambiru <dj70083798@gmail.com>
2024-01-19 13:41:27 +0900

    [원론] JSX 속성 데이터 바인딩(valid, invalid)

---
+++ src/lecture/12-web-component.js
@@ -0,0 +1,10 @@
+// 웹 컴포넌트 API(웹 표준)
+// globalThis.customElements.define('custom-element', CustomComponent)
+// <slot></slot>
+// <custom-element></custom-element>
+
+// 웹 컴포넌트에 끼워넣으려면 slot이라는 것을 사용해야한다.
+// <custom-element>
+//  <h2>headline</h2>
+//  <span>span element</span>
+// </custom-element>
--- src/lecture/lib/EUID.js
+++ src/lecture/lib/EUID.js
@@ -5,12 +5,14 @@ export function createElement(type, props = {}, ...children) {
   const element = document.createElement(type); // 'div', 'table'
   // props - 생성한 DOM 요소 노드에 속성 설정
   // props 객체를 배열로 변경해서 순환한 다음
-  const keyValues = Object.entries(props);
-  // 각 속성(key)과 속성 값(value)을 element에 설정한다. (반복)
-  // 문(statement)
-  // for (const [key, value] of keyValues) element.setAttribute(key, value);
-  // 식(expression) : 반환값이 없어도 된다.
-  keyValues.forEach(([key, value]) => element.setAttribute(key, value));
+  if (props && !Array.isArray(props) && props instanceof Object) {
+    const keyValues = Object.entries(props);
+    // 각 속성(key)과 속성 값(value)을 element에 설정한다. (반복)
+    // 문(statement)
+    // for (const [key, value] of keyValues) element.setAttribute(key, value);
+    // 식(expression) : 반환값이 없어도 된다.
+    keyValues.forEach(([key, value]) => element.setAttribute(key, value));
+  }
 
   // ...children - 생성한 DOM 요소 노드의 자식으로 구성
   // ...children을 순환해서 , 개별적으로 element의 자식으로 설정
