8f49d9ddbe49a086318c8e747c3a20205256d27e
bambiru <dj70083798@gmail.com>
2024-01-23 09:59:36 +0900

    [정리] 학습 주제 및 실습 자료

---
+++ src/lecture/19-reusable-component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import FormControl from '../components/FormCotrol';
+
+function App() {
+  return (
+    <div id="app">
+      <FormControl />
+      <FormControl />
+    </div>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
