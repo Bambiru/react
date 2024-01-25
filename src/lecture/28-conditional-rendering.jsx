692cfd4bf3b6498740c63ac8a94032a9dc04d0d3
bambiru <dj70083798@gmail.com>
2024-01-25 11:27:16 +0900

    [원론] JS 문법을 사용해서 리액트 조건부 렌더링
    1. 조건문
    2. 조건 연산자 식
    3. nullish 병합 연산자
    4. 옵셔널 체이닝

--- src/lecture/28-conditional-rendering.jsx
+++ src/lecture/28-conditional-rendering.jsx
@@ -1,19 +1,67 @@
-export default function Exercise() {
+export default function Exercise(/* nullish { isExist } */) {
   // 로그인 여부(상태 변수)
-  let isSignin = false; // Math.random() >= 0.5;
+  let isSignin = true; // Math.random() >= 0.5;
 
   // 로그인 여부에 따라 조건부 렌더링 마크업
-  let markup = <></>;
+  // let markup = <></>;
 
   // 서버 측 렌더링 (서버사이드렌더링) => 서버에서 그려옵니다.
   // 조건부 렌더링 (실제 DOM에 그리지 않는다.)
 
   // 클라이언트 측 렌더링 (클라이언트에서 그립니다.)
-  if (isSignin) {
-    markup = <div>대시보드 페이지 표시</div>;
-  } else {
-    markup = <div>로그인 페이지 표시</div>;
-  }
+  // 조건부 렌더링 (실제 DOM에 그리지 않는다.)
 
-  return markup;
+  // 1-1. if문 활용 렌더링
+  // if (isSignin) {
+  //   markup = <DashboardPage />;
+  // } else {
+  //   markup = <SignInPage />;
+  // }
+  // return markup;
+
+  // 1-2. switch 문 활용 렌더링
+  // switch (isSignin) {
+  //   case true:
+  //     return <DashboardPage />;
+  //   default:
+  //     return <SignInPage />;
+  // }
+
+  // 2.제어 흐름 연산자 식 사용 렌더링
+  // return (isSignin && <DashboardPage />) || <SignInPage />;
+
+  // 3. 터너리(삼항 연산자 식)
+  // return isSignin ? <DashboardPage /> : <SignInPage />;
+
+  // 4. nullish 병합 연산자 사용
+  // return isExist /* undefined, null */ ?? <SignInPage />;
+
+  // 5. 옵셔널 체이닝 사용
+  return renders.dashboardPage?.(); // undefined
+
+  /* 이렇게 해놨을 경우, 개발자도구를 이용해서 확인할 수 있다. */
+  //   return (
+  //     <>
+  //       <DashboardPage />
+  //       <SignInPage />
+  //     </>
+  //   );
+  // }
+}
+const renders = {
+  sign() {
+    return <SignInPage />;
+  },
+
+  dashboard() {
+    return <DashboardPage />;
+  },
+  // dashboardPage : undefined
+};
+
+function DashboardPage() {
+  return <div>대시보드 페이지 표시</div>;
+}
+function SignInPage() {
+  return <div>로그인 페이지 표시</div>;
 }
