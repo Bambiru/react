41db9df2fefbd1139102951d02bd2db0839d1529
bambiru <dj70083798@gmail.com>
2024-01-25 14:03:50 +0900

    [정리]유틸리티 함수
    -getStaticAsset
    -getStaticImage

--- src/lecture/29-a11y-hidden-component.jsx
+++ src/lecture/29-a11y-hidden-component.jsx
@@ -3,24 +3,16 @@ import { A11yHidden } from '../components';
 // src 안에 있는 자산(에셋, assets)을 번들러를 사용해서 화면에 표시하고,
 // 빌드했을 때 정상적으로 이미지를 빌드하려면 import로 호출해서 사용해야 한다.
 import bannerImage from '../assets/banner.png';
+import { getStaticImage } from '../utils/getStaticAsset';
 
 // 환경 변수
 console.log(import.meta.env);
-
-// 유틸리티 함수
-function getStaticAsset(assetPath) {
-  return import.meta.env.BASE_URL + assetPath;
-}
-
-function getStaticImage(imagePath) {
-  return;
-}
 
 function Exercise() {
   return (
     <figure>
       {/* public 정적 자산 */}
-      <img src={getStaticAsset('images/faces/man-03.jpg')} alt="" />
+      <img src={getStaticImage('faces/man-03.jpg')} alt="" />
       {/* 마치 정적인 것처럼 src 안의 자산 | 보여지는 건 잘 나오지만 빌드하게되면 깨지게 된다. */}
       <img src="/src/assets/banner.png" alt="" height={100} />
       {/* src/assets 동적 자산으로 호출(import) */}
