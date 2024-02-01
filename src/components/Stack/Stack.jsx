17301ec90a5be947a6657651ae6f2dcbf5f0615a
bambiru <dj70083798@gmail.com>
2024-02-01 10:38:52 +0900

    Stack 만들기

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -3,14 +3,42 @@ import classes from './Stack.module.css';
 function Stack({
   as: ComponentName = 'div',
   className: customClassNames = '',
+  style: 컴포넌트_사용자의_커스텀스타일,
   vertical = false,
+  gap,
+  my: marginBlock,
+  mx: marginInline,
   ...restProps /* {children} */
 }) {
-  const classNames = `${classes.Stack} ${customClassNames}`.trim();
+  const 컴포넌트_클래스이름 = `${classes.Stack} ${customClassNames}`.trim();
 
-  console.log(vertical);
+  const 컴포넌트_제작자의_스타일 = {};
+  if (vertical) {
+    컴포넌트_제작자의_스타일['--direction'] = 'column';
+  }
+  if (gap || gap === 0 /* falsy값이기때문에 지정해줘야 한다. */) {
+    컴포넌트_제작자의_스타일['--gap'] = `${gap}px`;
+  }
+  if (marginBlock || marginBlock === 0) {
+    컴포넌트_제작자의_스타일['--marginBlock'] = `${marginBlock}px`;
+  }
+  if (marginInline || marginInline === 0) {
+    컴포넌트_제작자의_스타일['--marginInline'] = `${marginInline}px`;
+  }
 
-  return <ComponentName className={classNames} {...restProps} />;
+  // <div style={{'--direction':'row' , ...customStyles }}/>
+  const 컴포넌트_스타일_옵션 = {
+    ...컴포넌트_제작자의_스타일,
+    ...컴포넌트_사용자의_커스텀스타일,
+  };
+
+  return (
+    <ComponentName
+      className={컴포넌트_클래스이름}
+      style={컴포넌트_스타일_옵션}
+      {...restProps}
+    />
+  );
 }
 
 export default Stack;
--- src/lecture/38-stack-layout-component.jsx
+++ src/lecture/38-stack-layout-component.jsx
@@ -1,11 +1,15 @@
 import { Stack } from '@/components';
-
+import './38-stack-layout-component.css';
 function Exercise() {
   return (
-    <Stack vertical gap={20}>
+    <Stack vertical gap={0} my={20} mx={20}>
       {/* vertical만 입력해주면 vertical={true}와 같다 */}
       <h2>Exercise</h2>
-      <p>스택</p>
+      <Stack>
+        <p>아이템1</p>
+        <p>아이템2</p>
+        <p>아이템3</p>
+      </Stack>
     </Stack>
   );
 }
